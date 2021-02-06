bfc9973fd9f02046fbfe4eb690e861116884ba34
Addison Freeman <addisonfreeman91@gmail.com>
2021-02-06 12:53:20 -0500

    init line chart for speedtests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Card, CardContent, Grid, Typography } from '@material-ui/core';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+import { API_HOST } from './const';
 
 const prepareChartData = (data) => {
   return data.map((datum) => {
@@ -7,13 +19,28 @@ const prepareChartData = (data) => {
   });
 };
 
+const formatTimestamp = (timestamp) => {
+  const date = new Date(timestamp * 1000).toLocaleTimeString();
+  return `${date.slice(0, date.lastIndexOf(':'))} ${date.slice(-2)}`;
+};
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#212529',
+    width: '100%',
+  },
+  card: {},
+}));
+
 const App = () => {
+  const classes = useStyles();
   const [data, setData] = useState();
 
   useEffect(() => {
     function getData() {
       return axios
-        .get('http://192.168.0.42:9000/tests/past-day')
+        .get(`${API_HOST}/tests/past-day`)
         .then((response) => {
           setData(prepareChartData(response.data['payload']));
         })
@@ -22,9 +49,59 @@ const App = () => {
     getData();
   }, []);
 
-  console.log('data: ', data);
-
-  return <div className='App'></div>;
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography className={classes.title} gutterBottom>
+                Internet Speed
+              </Typography>
+              <Box height={300} position='relative'>
+                <ResponsiveContainer minHeight='250' minWidth='250'>
+                  <LineChart width={500} height={400} data={data}>
+                    <XAxis
+                      dataKey='timestamp'
+                      padding={{ left: 30, right: 30 }}
+                      tickFormatter={formatTimestamp}
+                    />
+                    <YAxis />
+                    <Tooltip
+                      labelFormatter={formatTimestamp}
+                      formatter={(value, name, props) => {
+                        if (['download', 'upload'].includes(name))
+                          return `${value} Mbps`;
+                        return `${value} ms`;
+                      }}
+                    />
+                    <Line
+                      dot={false}
+                      type='monotone'
+                      dataKey='download'
+                      stroke='#8884d8'
+                    />
+                    <Line
+                      dot={false}
+                      type='monotone'
+                      dataKey='upload'
+                      stroke='#fca311'
+                    />
+                    <Line
+                      dot={false}
+                      type='monotone'
+                      dataKey='ping'
+                      stroke='#941c2f'
+                    />
+                  </LineChart>
+                </ResponsiveContainer>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/const.js
@@ -0,0 +1,1 @@
+export const API_HOST = 'http://192.168.0.42:9000';
